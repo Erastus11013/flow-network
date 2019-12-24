26b2c4f755c2137284b88aec38dd9e1b1e864f2f
Erastus11013 <erastuswagwan@gmail.com>
2019-12-23 20:42:53 -0500

    Update dinitz.py

--- dinitz.py
+++ dinitz.py
@@ -1,96 +1,106 @@
-from ford_fulkerson import flow_network
+from FlowNetwork.graph import FlowNetwork
 from collections import deque
 from math import inf
 
 
-class layered_graph(flow_network):
+class LayeredGraph(FlowNetwork):
     """For implementation of dinitz's algorithm"""
-    MAX_LEVELS = 13
+
 
     def __init__(self):
-        flow_network.__init__(self)
-        self.layers: List[set] = [0] * layered_graph.MAX_LEVELS  # set of layers
+        FlowNetwork.__init__(self)
+        self.layers = {}  # set of layers
 
     def create_layered_graph(self, source, sink):
         """Creates a layered graph/ admissible graph using breadth first search
-
+            Variables:
+                delta: the level of the sink
+                lid: the level id
         """
         visited = {node: False for node in self.nodes}
         visited[source] = True
-        lid, layer, frontier = 0, set(), [source]
-        self.layers[lid] = set(source)
+        lid, frontier = 0, [source]
         delta = None
 
         while frontier:
             lid += 1
             next_layer = []
             for u in frontier:
+                self.layers[u] = set()
                 for v in self.neighbors_in_residual_graph(u):
                     if not visited[v]:
-                        if v == sink: delta = lid
-                        layer.add(v)
+                        if v == sink:
+                             delta = lid
                         visited[v] = True
                         next_layer.append(v)
-            if layer: self.layers[lid] = layer
-            layer = set()
+                        self.layers[u].add(v)
             frontier = next_layer
         return delta
 
-    def __delete_from_layer_graph(self, pred, sink):
-        """Assumes the pred dict is ordered"""
-        j = 1
-        for k in pred:
-            if k != sink:
-                self.layers[j].remove(k)
-                j += 1
-            else:
-                return True
+    def _delete_saturated_edges(self, path):
+        """The path is composed of edges from the source to the sink
+        Deletes saturated edges from the layered graph L."""
+        for lid, edge in enumerate(path):
+            src, dest, _, _ = edge
+            if self.residual_capacity(src ,dest) == 0:  # edge is saturated
+                self.layers[src].remove(dest)  # delete the edge
+        return True
 
-    def saturate_one_path(self, source, start, sink, delta, print_path=True):
-        """Uses Iterative DFS
-          After reaching the end, destroy the whole path from the beginning. How do we do that?
-            Multiple dfs searches
+    def saturate_one_path(self, source, sink, delta, print_path=True):
+        """Traverses the graph using a modified non-recusrive dfs to find a find a flow to saturate
+            one path in the layered graph.
             If no s -> t path exists, this function won't be called
-        O(V + E)"""
-        visited, stack, pred = set(), [start], {start: source}  # O(1)
-        lid = 2
+            delta is the level of the sink
+            O(|V||E|) time
+        """
 
-        while stack:   # O(V)
-            u = stack.pop()
-            if lid > delta:  # the sink is unreachable
-                return 0
-            if u not in visited:
-                visited.add(u)
-                for v in self.neighbors_in_residual_graph(u):
-                    if v not in visited and v in self.layers[lid]:
-                        if v == sink: # that's it
-                            pred[v] = u
-                            cf = self._augment(pred, source, sink, print_path)
-                            self.__delete_from_layer_graph(pred, sink)
-                            return cf
+        S, pred = set(), [source]  # O(1)
+        lid = 0 
+        cf = None
+
+        while S:
+            u = S.pop()
+            if self.layers[u]:  # advance if vertex u has outgoing edges
+                for v in self.layers[u]:  # for each vertex v in neighbors_L(u)
+                    if v == sink: # augment the path by the lowest residual capacity
                         pred[v] = u
-                        stack.append(v)
-                        lid += 1
-        return 0
+                        cf, path = self._augment(pred, source, sink, print_path)  # augment path
+                        # delete saturated edges
+                        self._delete_saturated_edges(path)
+                        # start the dfs from the source again
+                        S.clear()  # clear the stack and stop
+            else:  # retreat
+                if u == source:  # if u is the source, blocking flow has been attained
+                    S.clear()
+                else:
+                    # delete last edge from the layered graph
+                    self.layers[u].pop()
+        return cf
+        
+
 
     def find_blocking_flow(self, source, sink, delta):
+        """ prodedure ModifiedDFS:
+                1 advance(v, w): move from v to w for (v, w) ∈ L,
+                2 retreat(u, v): if (v, w) ∄ L∀w, then delete (u, v) from L
+                3 augment: if v = t, augment f along the minimum residual flow on the
+                found s-t path and delete saturated edges.
+        """
         bf = 0
-        for u in self.neighbors_in_residual_graph(source):
-            bf += self.saturate_one_path(source, u, sink, delta, True)
+        while len(self.layes[source]):  # blocking flow exists
+            bf += self.saturate_one_path(source, sink, delta, print_path=True)
         return bf
-
-    def sink_is_reachable(self, delta, sink):
-        return sink in self.layers[delta]
 
     def dinitz_algorithm(self, source=None, sink=None):
         """
-        1: f(v,w) ← 0 ∀(v,w) ∈ E
-        2: Create residual graph Gf
-        3: Create a layered graph Lf using BFS...
-        4: while Gf has an s-t path do
-        5:      Find a blocking flow f in Gf by multiple dfs runs
-        6:      Augment by f
-        5: end while
+        procedure Dinic:
+            1 f(v,w) ← 0 ∀(v,w) ∈ E
+            2 Create residual graph Gf
+            3 Create a layered graph Lf using BFS...
+            4 while Gf has an s-t path do
+            5      Find a blocking flow f in Gf by multiple dfs runs
+            6      Augment by f
+            5 end while
         """
         if source is None: source = self.supersource
         if sink is None: sink = self.supersink
@@ -110,7 +120,7 @@ class layered_graph(flow_network):
         return self.get_max_flow(source)
 
 def test_dinitz(nodes, edges):
-    g1 = layered_graph()
+    g1 = LayeredGraph()
     g1.add_nodes(nodes)
     g1.add_edges(edges)
     v1 = g1.dinitz_algorithm('s', 't')
@@ -127,4 +137,3 @@ e = [(s, a, 5), (s, d, 10), (s, g, 15), (a, b, 10), (b,c, 10), (b,e, 25), (c, t,
 
 if __name__ == '__main__':
     test_dinitz(n ,e)
-
