f487f7e38ca52fda83324908de8bba0c76a60d6b
Erastus Murungi <erastuswagwan@gmail.com>
2020-03-09 14:46:18 -0400

    Update push_relabel.py

--- push_relabel.py
+++ push_relabel.py
@@ -1,4 +1,5 @@
 from typing import Dict, Tuple, Union, Iterable
+from types import FunctionType
 from collections import defaultdict, deque
 from functools import reduce
 from ctypes import Structure, c_int, c_float
@@ -17,7 +18,7 @@ class EdgeInfo(Structure):
     _fields_ = [("cap", c_int), ("flow", c_int), ("weight", c_float)]
 
     def __repr__(self):
-        return "EdgeInfo (C:%.2d F:%.2d)" % (self.cap, self.flow)
+        return "(C:%.2d F:%.2d)" % (self.cap, self.flow)
 
 
 Graph = Dict[Node, Dict[Node, EdgeInfo]]
@@ -25,20 +26,22 @@ ResidualGraph = Dict[Node, Dict[Node, float]]
 
 excess = defaultdict(lambda: 0)
 height = None
+inqueue = defaultdict(lambda: False)
+seen = defaultdict(lambda: 0)
 
 
-def edge_in_graph(src: Node, dst: Node, g: Graph):
+def edge_in_graph(src: Node, dst: Node, g: Graph) -> bool:
     if src not in g:
         return False
     return dst in g[src]
 
 
-def edges(g: Graph):
+def edges(g: Graph) -> Iterable[Tuple[Node, Node]]:
     a = [tuple((n, neighbor) for neighbor in adjacency(g, n)) for n in g]
     return chain(*a)
 
 
-def insert_edges_from_iterable(g: Graph, edges: Iterable[tuple]):
+def insert_edges_from_iterable(g: Graph, edges: Iterable[tuple]) -> None:
     """Assumes that the nodes are in the order.
             (src, dst, attributes)
     """
@@ -89,7 +92,8 @@ def residual_capacity(g, u, v) -> int:
     return capacity(g, u, v) - flow(g, u, v)
 
 
-def dijkstra(g, source, target=None, weight_function=None) -> Dict[Node, float]:
+def dijkstra(g: Union[Graph, ResidualGraph], source: Node,
+             target: Node = None, weight_function: FunctionType = None) -> Dict[Node, float]:
     pred, distance = defaultdict(lambda: None), defaultdict(lambda: int('inf'))
     distance[source] = 0
     W = weight if weight_function is None else weight_function
@@ -104,7 +108,6 @@ def dijkstra(g, source, target=None, weight_function=None) -> Dict[Node, float]:
                 distance[u] = distance[v] + W(u, v, g)
                 heappush((distance[v], v))
                 pred[v] = u
-
     return distance
 
 
@@ -117,12 +120,12 @@ def residual_graph(g: Graph) -> ResidualGraph:
     return rg
 
 
-def rand_flow_cap(lim) -> tuple:
+def rand_flow_cap(lim: int) -> tuple:
     cap = random.randint(2, lim)
     return cap, random.randint(1, cap)
 
 
-def node_is_active(u, source, sink) -> bool:
+def node_is_active(u: Node, source: Node, sink: Node) -> bool:
     return u != source and u != sink and excess[u] > 0
 
 
@@ -135,98 +138,139 @@ def bfs(graph: Union[ResidualGraph, Graph], s: Node) -> Dict[Node, float]:
         for v in adjacency(graph, u):
             if not distance[v]:
                 distance[v] = distance[u] + 1
+                Q.appendleft(v)
     return distance
 
 
-def shallow_reverse(g) -> Dict[Node, Dict[Node, int]]:
+def shallow_reverse(g: Union[Graph, ResidualGraph]) -> Dict[Node, Dict[Node, int]]:
     revg = defaultdict(dict)
-    for u in g:
-        for v in adjacency(g, u):
-            revg[v][u] = 0
+    for u, v in edges(g):
+        revg[v][u] = 0
     return revg
 
 
-def exists_in_residual_graph(g: Graph, u: Node, v: Node) -> bool:
-    if edge_in_graph(u, v, g):
-        return g[u][v].cap - g[u][v].flow > 0
-    return False
-
-
-def get_active_node():
-    pass
-
-
-def generic_push_relabel(graph: Graph, source: Node, sink: Node) -> float:
-    """
-    Invariants:
-     1. h(s) = n at all times (where n = |V|);
-     2. h(t) = 0;
-     3. for every edge (v, w) of the current residual network (with positive residual capacity), h(v) ≤ h(w) + 1.
-
-     excess(u) = flow(into) - flow(out)
-    """
-
+def initialize_preflow(graph: Graph, source: Node, sink: Node) -> Graph:
     assert graph is not None and source in graph
-
     g = deepcopy(graph)
     global height
+
+    for u, v in edges(g):
+        g[u][v].flow = 0
+
     # saturate all the edges coming out of the source
     for v in adjacency(g, source):
         excess[v] = g[source][v].flow = g[source][v].cap
         excess[source] -= g[source][v].cap
 
-    # initialize heights
     for u, v in edges(g):
-        # insert reverse edges
-        if g[u][v].cap - g[u][v].flow > 0:
-            insert_edge(g, (v, u, g[u][v].cap, g[u][v].cap - g[u][v].flow))
-    height = bfs(shallow_reverse(g), sink)
+        insert_edge(g, (v, u, g[u][v].cap, residual_capacity(g, u, v), 1))
+
+    # Initialize heights as the shortest distance from the sink to every node except the source
+    # We perform bfs on the original graph, not the residual one
+    height = bfs(shallow_reverse(graph), sink)
     height[source] = num_nodes(g)
 
-    # create the the residual graph of g, Q contains the nodes with positive excesses
+    # Create the the residual graph of g, Q contains the nodes with positive excesses
     # Initially, those are just the edges outgoing from the source
-    Q = [(-height[u], u) for u in adjacency(g, source)]
+    return g
+
+
+def fifo_push_relabel(graph: Graph, source: Node, sink: Node) -> Tuple[float, Graph]:
+    """
+        FIFO Push/Relabel
+        Heuristics used:
+            1. Choosing highest vertex
+            2. Initializing the heights to shortest v-t paths
+        Invariants:
+            1. h(s) = n at all times (where n = |V|);
+            2. h(t) = 0;
+            3. for every edge (v, w) of the current residual network (with positive residual capacity), h(v) ≤ h(w) + 1.
+        excess(u) = flow(into) - flow(out)
+        residual_capacity(u, v) = capacity(u, v) - flow(u, v)
+
+    """
+    g = initialize_preflow(graph, source, sink)
+    Q = []
+    for u in adjacency(g, source):
+        inqueue[u] = True
+        heappush(Q, (-height[u], u))
+
     while Q:
+        # highest active node, has the lowest -(height)
         u = heappop(Q)[1]
-        if excess[u] and u != sink and u != source:
-            if not _push(g, u, Q):
-                _relabel(g, u, Q)
+        inqueue[u] = False
+        for v in adjacency(g, u):
+            if excess[u] == 0:
+                break
+            if height[u] == height[v] + 1 and residual_capacity(g, u, v) > 0:
+                push(g, u, v)
+                if not inqueue[v] and v not in (source, sink):
+                    heappush(Q, (-height[v], v))
+                    inqueue[v] = True
+        if excess[u] > 0:
+            relabel(g, u)
+            heappush(Q, (-height[u], u))
+            inqueue[u] = True
 
     return sum(g[source][v].flow for v in adjacency(g, source))
 
 
-def _push(g: Graph, u: Node, queue) -> bool:
+def relabel_to_front(graph: Graph, source: Node, sink: Node) -> float:
+    """Relabel to front algorithm"""
+
+    # list of valid nodes
+    L = list(filter(lambda node: node not in (sink, source), graph.keys()))
+    g = initialize_preflow(graph, source, sink)
+    p, n = 0, len(L)
+
+    while p < n:
+        u = L[p]
+        old_height = height[u]
+        discharge(g, u)
+        if height[u] > old_height:
+            L.insert(0, L.pop(p))  # move to front
+            p = 0
+        else:
+            p += 1
+
+    return sum(g[source][v].flow for v in adjacency(g, source))
+
+
+def push(g: Graph, u: Node, v: Node):
     """ Push(u). If ∃v with admissible arc (u, v) ∈ E_f , then send flow δ := min(cf (uv), ef (u))
         from u to v. Note that this causes excess ef (u) to fall by δ, and excess ef (v) to increase
             by δ. If δ = cf (uv), this is called a saturating push, else it is an non-saturating push.
     """
-
-    pushed = False
-    for v in adjacency(g, u):
-        if excess[u] > 0 and height[u] == height[v] + 1:
-            pushed = True
-            delta = min(g[u][v].cap - g[u][v].flow, excess[u])
-            if edge_in_graph(u, v, g):
-                g[u][v].flow += delta
-            else:
-                g[v][u].flow -= delta
-            excess[u] -= delta
-            excess[v] += delta
-            # excess[v] must be > 0
-            if v != u:
-                heappush(queue, (-height[v], v))
-            if excess[u] == 0:
-                return True
-    return pushed
+    assert excess[u] > 0 and height[u] == height[v] + 1
+    delta = min(residual_capacity(g, u, v), excess[u])
+    g[u][v].flow += delta
+    g[v][u].flow -= delta
+    excess[u] -= delta
+    excess[v] += delta
 
 
-def _relabel(g: Graph, u: Node, queue):
-    valid = [v for v in adjacency(g, u) if g[u][v].cap - g[u][v].flow > 0]
+def relabel(g: Graph, u: Node):
+    valid = [v for v in adjacency(g, u) if residual_capacity(g, u, v) > 0]
+    assert (len(valid) != 0)
+    assert excess[u] > 0 and all(height[u] <= height[v] for v in valid)
     height[u] = min(height[v] for v in valid) + 1
-    heappush(queue, (-height[u], u))
 
 
-def test(nn):
+def discharge(g: Graph, u: Node):
+    neighbors = adjacency(g, u)
+    while excess[u] > 0:
+        if seen[u] < len(neighbors):
+            v = neighbors[seen[u]]
+            if residual_capacity(g, u, v) > 0 and height[u] == height[v] + 1:
+                push(g, u, v)
+            else:
+                seen[u] += 1
+        else:
+            relabel(g, u)
+            seen[u] = 0
+
+
+def test(nn: int):
     g = defaultdict(dict)
     rand_nodes = random.randint(0, 100, nn)
     e = list((choice(rand_nodes), choice(rand_nodes),
@@ -236,23 +280,47 @@ def test(nn):
     pprint(residual_graph(g))
 
 
+# s, t, a, b, c, d, e, f, g, h, i = 's', 't', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'
+# # n = [s, t, a, b, c, d, e, f, g, h, i]
+# e = [(s, a, 5), (s, d, 10), (s, g, 15), (a, b, 10), (b, c, 10), (b, e, 25), (c, t, 5), (d, a, 15), (d, e, 20),
+#         (e, f, 30), (e, g, 5),
+#         (f, t, 15), (f, b, 15), (f, i, 15), (g, h, 25), (h, i, 10), (h, f, 20), (i, t, 10)]
+
+s, v1, v2, v3, v4, v5, t = 's', 'v1', 'v2', 'v3', 'v4', 'v5', 't'
+# n1 = [s, v1, v2, v3, v4, t]
+e = [(s, v1, 16), (s, v2, 13), (v1, v3, 12), (v2, v1, 4), (v2, v4, 14), (v3, v2, 9), (v3, t, 20),
+     (v4, v3, 7), (v4, t, 4)]
+
+
 def test_generic_push_relabel():
-    s, t, a, b, c, d, e, f, g, h, i = 's', 't', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'
-    # n = [s, t, a, b, c, d, e, f, g, h, i]
-    e = [(s, a, 5), (s, d, 10), (s, g, 15), (a, b, 10), (b, c, 10), (b, e, 25), (c, t, 5), (d, a, 15), (d, e, 20),
-         (e, f, 30), (e, g, 5),
-         (f, t, 15), (f, b, 15), (f, i, 15), (g, h, 25), (h, i, 10), (h, f, 20), (i, t, 10)]
-
-    s, v1, v2, v3, v4, v5, t = 's', 'v1', 'v2', 'v3', 'v4', 'v5', 't'
-    # n1 = [s, v1, v2, v3, v4, t]
-    e1 = [(s, v1, 16), (s, v2, 13), (v1, v3, 12), (v2, v1, 4), (v2, v4, 14), (v3, v2, 9), (v3, t, 20),
-          (v4, v3, 7), (v4, t, 4)]
-
     g = defaultdict(dict)
-    insert_edges_from_iterable(g, e1)
-    maxflow = generic_push_relabel(g, s, t)
-    print(maxflow)
+    insert_edges_from_iterable(g, e)
+    mf = fifo_push_relabel(g, s, t)
+    # print(mf)
+
+
+def test_relabel_to_front():
+    g = defaultdict(dict)
+    insert_edges_from_iterable(g, e)
+    mf = relabel_to_front(g, s, t)
+    # print(mf)
 
 
 if __name__ == "__main__":
-    test_generic_push_relabel()
+    from time import perf_counter
+    num_iter = 100
+    t1 = perf_counter()
+    for _ in range(num_iter):
+        excess = defaultdict(lambda: 0)
+        inqueue = defaultdict(lambda: False)
+        height = None
+        test_generic_push_relabel()
+    print(f"FIFO push relabel ran in: {perf_counter() - t1:.2f} seconds.")
+
+    t1 = perf_counter()
+    for _ in range(num_iter):
+        excess = defaultdict(lambda: 0)
+        height = None
+        seen = defaultdict(lambda: 0)
+        test_relabel_to_front()
+    print(f"Relabel to front ran in: {perf_counter() - t1:.2f} seconds.")
