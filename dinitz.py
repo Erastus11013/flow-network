c93853644162b066f294f7a21628aec68cb70695
Erastus Murungi <erastuswagwan@gmail.com>
2020-03-18 00:08:11 -0400

    Update dinitz.py

--- dinitz.py
+++ dinitz.py
@@ -1,4 +1,4 @@
-from graph import FlowNetwork
+from edmonds_karp import FlowNetwork
 from collections import defaultdict
 
 
@@ -7,7 +7,7 @@ class LayeredGraph(FlowNetwork):
 
     def __init__(self):
         FlowNetwork.__init__(self)
-        self.layers = {}  # set of layers
+        self.layers = defaultdict(set)  # set of layers
 
     def create_layered_graph(self, source, sink):
         """Creates a layered graph/ admissible graph using breadth first search
@@ -17,25 +17,25 @@ class LayeredGraph(FlowNetwork):
         """
         visited = defaultdict(lambda: False)
         visited[source] = True
-        lid, frontier = 0, [source]
-        delta = None
+        lid, frontier, next_layer = 0, [source], []
+        sink_reached = False
 
         while frontier:
-            lid += 1
-            next_layer = []
             for u in frontier:
                 self.layers[u] = set()
                 for v in self.neighbors_in_residual_graph(u):
                     if not visited[v]:
                         if v == sink:
-                            delta = lid
+                            sink_reached = True
                         visited[v] = True
                         next_layer.append(v)
                         self.layers[u].add(v)
-            frontier = next_layer
-        return delta
+            frontier.clear()
+            frontier.extend(next_layer)
+            next_layer.clear()
+        return sink_reached
 
-    def _delete_saturated_edges(self, cf, path):
+    def del_saturated_edges(self, cf, path):
         """Deletes saturated edges from the layered/level graph L."""
         for lid, edge in enumerate(path):
             src, dest, cap, _ = edge
@@ -43,9 +43,8 @@ class LayeredGraph(FlowNetwork):
                 self.layers[src].remove(dest)  # delete the edge
         return True
 
-    def saturate_one_path(self, source, sink, print_path=True):
-        """Traverses the graph using a modified non-recursive DFS to find a find a flow to saturate
-            one path in the layered graph.
+    def find_blocking_flow(self, source, sink):
+        """ Finds a blocking flow of the layered graph by saturating one path at time.
             If no s -> t path exists, this function won't be called delta is the level of the sink.
             The algorithm runs in O(|V||E|) time.
 
@@ -56,47 +55,34 @@ class LayeredGraph(FlowNetwork):
                 found s-t path and delete saturated edges.
             Variables:
              S: a stack to simulate DFS of the graph.
-             pred: a dictionary where pred[u] = v tells that u is v's predecessor.
-        """
+             pred: a dictionary where pred[u] = v tells that u is v's predecessor."""
 
-        S, pred = [source], {}  # O(1)
-        cf = 0
-
-        while S:
-            u = S.pop()
-            if u in self.layers:
-                if self.layers[u]:  # advance if vertex u has outgoing edges
+        while True:  # blocking flow exists
+            S, pred = [source], {}  # O(1)
+            cf = 0
+            while S:
+                u = S.pop()
+                # advance if vertex u has outgoing edges
+                if len(self.layers[u]) > 0:
                     for v in self.layers[u]:  # for each vertex v in neighbors_L(u)
                         if v == sink:  # augment the path by the lowest residual capacity
                             pred[v] = u
-                            cf, path = self._augment(pred, source, sink, print_path)  # augment path
+                            cf, path = self.update_network(pred, source, sink)  # augment path
                             # delete saturated edges
-                            self._delete_saturated_edges(cf, path)
+                            self.del_saturated_edges(cf, path)
                             # start the dfs from the source again
                             S.clear()
-                            break  # clear the stack and stop
+                            break
                         else:
                             S.append(v)  # push to stack
                             pred[v] = u
-
-                else:  # retreat
-                    if u == source:  # if u is the source, blocking flow has been attained
-                        S.clear()
-                    else:
-                        # delete last edge from the layered graph
-                        self.layers.pop(u)
-        return cf
-
-    def find_blocking_flow(self, source, sink):
-        """ Finds a blocking flow of the layered graph by saturating one path at time. """
-        bf = 0
-        while len(self.layers[source]):  # blocking flow exists
-            cf = self.saturate_one_path(source, sink, print_path=False)
-            if cf:
-                bf += cf
-            else:
-                return bf
-        return bf
+                # retreat
+                else:
+                    # if u is the source, blocking flow has been attained
+                    # delete this edge from the layered graph
+                    self.layers.pop(u)
+            if not cf:
+                break
 
     def dinitz_algorithm(self, source=None, sink=None):
         """
@@ -109,29 +95,12 @@ class LayeredGraph(FlowNetwork):
             6      Augment by f
             5 end while
         """
-        if source is None:
-            source = self.supersource
-        if sink is None:
-            sink = self.supersink
-        if source not in self.nodes:
-            return None
 
-        total_flow = 0
         self.set_flows(0)  # step 1
-        self.create_residual_graph()  # initialization
-        delta = self.create_layered_graph(source, sink)  # initialization
+        self.build_residual_graph()  # initialization
+        sink_reachable = self.create_layered_graph(source, sink)  # initialization
 
-        if delta is None:  # if the sink is unreachable, max flow is already attained
-            return self.get_max_flow(source)
-
-        while delta is not None:
-            blocking_flow = self.find_blocking_flow(source, sink)
-            if blocking_flow == 0:  # max flow has been reached
-                return self.get_max_flow(source)
-
-            total_flow += blocking_flow
-            self.create_residual_graph()
-            delta = self.create_layered_graph(source, sink)
-            assert total_flow == self.get_max_flow(source)
-
-        return self.get_max_flow(source)
+        while sink_reachable:
+            self.find_blocking_flow(source, sink)
+            sink_reachable = self.create_layered_graph(source, sink)
+        return self.maxflow(source)
